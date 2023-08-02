b70836be9015eb4e35fa7590e282c45a4bd5cccd
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-08-02 01:55:02 -0600

    Implementacion de event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mi primer proyecto de angular 2023';
-  userName = 'Francisco Santiago';
-
-  textPlaceHolder = 'Escriba algo en este input';
-  inputEnabled = true;  
-  imgSource = "https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png";
-
-
-  constructor() {
-    setInterval(() => {
-      this.userName = this.userName == 'Francisco Santiago' ? 'Nombre de usuario dinamico' : 'Francisco Santiago';
-
-      this.inputEnabled = false;
-    }, 3000);
-  }
-
-  getSuma(num1: number, num2: number) {
-    return num1 + num2;
+  title = 'Este es un ejemplo de event binding';
+  
+  cambiarTexto() : void {
+    this.title = 'En el proximo video vamos a ver Two way data-binding';
   }
 }
