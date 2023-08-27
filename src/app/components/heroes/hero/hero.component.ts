da2862826dea75c25f4e5437bc2461137c9ac787
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-08-27 00:22:32 -0600

    Recapitulando One y two way data binding

--- src/app/components/heroes/hero/hero.component.ts
+++ src/app/components/heroes/hero/hero.component.ts
@@ -9,4 +9,12 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} tiene ${this.age} años`;
+  }
+
 }
