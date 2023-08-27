af4ff9e4265ad7ff143877b3e663f2044cad8722
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-08-27 17:09:28 -0600

    reestructurando archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterModule } from './components/counter/counter.module';
-import { HeroesModule } from './components/heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
