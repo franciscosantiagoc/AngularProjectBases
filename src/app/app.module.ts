6df292cca306a6d1a3f050a038a6b497ca16f9b1
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-08-27 13:45:28 -0600

    Uso y manejo de modulos en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { HeroComponent } from './components/heroes/hero/hero.component';
-import { ListComponent } from './components/heroes/list/list.component';
+import { CounterModule } from './components/counter/counter.module';
+import { HeroesModule } from './components/heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
