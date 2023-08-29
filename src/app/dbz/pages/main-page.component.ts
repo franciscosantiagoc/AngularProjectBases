023f3d4d877e3c4140ea512c461ec6eef098160e
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-08-28 22:52:24 -0600

    Uso de la directiva Input para pasar datos component padre -> hijo

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,4 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 3000
+    },
+    {
+      name: 'Goku',
+      power: 10000
+    },
+    {
+      name: 'Piccoro',
+      power: 7000
+    },
+    {
+      name: 'Gohan',
+      power: 4000
+    }
+  ];
 }
